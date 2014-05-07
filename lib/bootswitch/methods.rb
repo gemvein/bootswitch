948e30d9e9d7d4c3e45185e777bd4f1149646a5a
Karen Lundgren <karen.e.lundgren@gmail.com>
2014-05-07 13:51:09 -0600

    Fixed method location

--- lib/bootswitch/methods.rb
+++ lib/bootswitch/methods.rb
@@ -2,4 +2,12 @@ module Bootswitch
   def self.themes
     configuration.themes
   end
+
+  def theme_name
+    send(Bootswitch.configuration.theme_method) || Bootswitch.configuration.default_theme
+  end
+
+  def theme_stylesheet_link_tag
+    render(partial: 'theme/stylesheet_link_tag')
+  end
 end
