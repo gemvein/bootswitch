96b0b30db65ad652f27ee09d11d8b1ba90cc47b7
Karen Lundgren <karen.e.lundgren@gmail.com>
2014-05-07 10:52:24 -0600

    Fixed bug with asset pipeline.

--- lib/bootswitch.rb
+++ lib/bootswitch.rb
@@ -2,10 +2,8 @@ module Bootswitch
   require 'rails'
   require 'haml-rails'
 
-  require 'bootswitch/engine'
   require 'bootswitch/configuration'
   require 'bootswitch/methods'
-  require 'bootswitch/railtie'
   require 'bootswitch/version'
 
   require 'bootstrap-sass'
--- lib/bootswitch/engine.rb
+++
@@ -1,5 +0,0 @@
-module Bootswitch
-  class Engine < Rails::Engine 
-    # isolate_namespace Bootswitch
-  end 
-end
